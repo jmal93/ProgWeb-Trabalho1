2765aa69f79441ae60f5982dfb6ee757f743786b
João Marcello <marcello.jao1611@gmail.com>
2025-09-24 23:31:22 -0300

    Conserto do modelo Usuário e __str__ de Review

--- biblioteca/gamerboxd/models.py
+++ biblioteca/gamerboxd/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Usuario(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100, help_text="Insira o nome")
@@ -9,32 +9,36 @@ class Usuario(models.Model):
     def __str__(self):
         return self.nome
 
+
 class Jogo(models.Model):
     id = models.AutoField(primary_key=True)
     nome = models.CharField(max_length=100, help_text="Insira o nome do jogo")
-    genero = models.EmailField(max_length=254, help_text="Insira o genero do jogo")
-    desenvolvedora = models.EmailField(max_length=254, help_text="Insira a desenvolvedora do jogo")
-    dtNasc = models.DateField(
+    genero = models.CharField(
+        max_length=20, help_text="Insira o genero do jogo")
+    desenvolvedora = models.CharField(
+        max_length=100, help_text="Insira a desenvolvedora do jogo")
+    dtLanc = models.DateField(
         help_text='Data de lancamento no formato DD/MM/AAAA',
         verbose_name='Data de lancamento')
 
-
     def __str__(self):
         return self.nome
-    
+
 
 class Review(models.Model):
     id_jogo = models.ForeignKey(Jogo, on_delete=models.CASCADE)
     id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     nota = models.IntegerField(help_text="Insira a nota")
-    descricao = models.CharField(max_length=256, help_text="Insira a descricao")
+    descricao = models.CharField(
+        max_length=256, help_text="Insira a descricao")
 
     class Meta:
         constraints = [
             models.UniqueConstraint(
-                fields=['id_jogo', 'id_usuario'], name='unique_jogo_usuario_combination'
+                fields=['id_jogo', 'id_usuario'],
+                name='unique_jogo_usuario_combination'
             )
         ]
 
     def __str__(self):
-        return (self.id_jogo, self.id_usuario)
+        return f"Review de {self.id_usuario} para {self.id_jogo}"
