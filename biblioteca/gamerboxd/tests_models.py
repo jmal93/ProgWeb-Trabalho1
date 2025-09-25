2e4f5774b9c101ed45d18944a00c2796863135c7
João Marcello <marcello.jao1611@gmail.com>
2025-09-24 23:32:16 -0300

    Testes dos modelos

---
+++ biblioteca/gamerboxd/tests_models.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from gamerboxd.models import Jogo, Usuario, Review
+
+
+class UsuarioModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Usuario.objects.create(nome="Joao", email="teste@teste.com")
+
+    def test_retorna_nome(self):
+        usuario = Usuario.objects.get(id=1)
+        nome_esperado = f'{usuario.nome}'
+        self.assertEqual(nome_esperado, str(usuario))
+
+
+class JogoModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Jogo.objects.create(nome='GTA', genero="Acao",
+                            desenvolvedora="Rockstar", dtLanc="2013-09-07")
+
+    def test_retorna_nome(self):
+        jogo = Jogo.objects.get(id=1)
+        nome_esperado = f'{jogo.nome}'
+        self.assertEqual(nome_esperado, str(jogo))
+
+
+class ReviewModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.usuario = Usuario.objects.create(
+            nome="Joao", email="teste@teste.com")
+        cls.jogo = Jogo.objects.create(nome='GTA', genero="Acao",
+                                       desenvolvedora="Rockstar", dtLanc="2013-09-07")
+
+        Review.objects.create(id_jogo=cls.jogo, id_usuario=cls.usuario,
+                              nota=10, descricao="teste")
+
+    def test_retorna_nome(self):
+        review = Review.objects.get(id=1)
+        nome_esperado = f'Review de {review.id_usuario} para {review.id_jogo}'
+        self.assertEqual(nome_esperado, str(review))
